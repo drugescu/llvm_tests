dcb6486b34bfe51b718ed5710a196d0afcb715d9
Dragos R. Rugescu <dragos.rugescu@yahoo.com>
2020-11-12 16:10:25 +0200

    Redid make script to output more stuff.

--- hello-world/do.sh
+++ hello-world/do.sh
@@ -1,17 +1,30 @@
-# llvm flags
+# Remove artifacts
+rm -f output.ll output.bc output.s
+
+# LLVM flags
 FLAGS="`llvm-config --cxxflags --ldflags --system-libs --libs all`"
 
-# compile to binary
+# Compile to binary
 clang++ -g -o hello_world $FLAGS hello_world.cpp
 
-# run binary
+# Run binary to output LLVM IR
 ./hello_world > output.ll
 
+# Emit assembly from LLVM IR with llc
+llc output.ll -o output.s
+
 # Emit binary with gas
-llvm-as output.ll -o output.elf
-chmod +x output.elf
+llvm-as output.ll -o output.bc
+chmod +x output.bc
 
-# Run program
-./output.elf
+# Run program .bc
+echo "------------- Output of compiled .bc file ---------------"
+./output.bc
 
+# Echo .ll
+echo "-------------------- LLVM IR output ---------------------"
+cat output.ll
 
+# Echo .s
+echo "----------------- LLVM assembly output ------------------"
+cat output.s
--- hello-world/output.s
+++ hello-world/output.s
@@ -1,12 +1,26 @@
-; ModuleID = 'asdf'
-source_filename = "asdf"
+	.text
+	.file	"asdf"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:                                # %entrypoint
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	movl	$.L__unnamed_1, %edi
+	callq	puts
+	popq	%rax
+	.cfi_def_cfa_offset 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L__unnamed_1,@object           # @0
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L__unnamed_1:
+	.asciz	"hello world!\n"
+	.size	.L__unnamed_1, 14
 
-@0 = private unnamed_addr constant [14 x i8] c"hello world!\0A\00", align 1
-
-define void @main() {
-entrypoint:
-  %0 = call i32 @puts(i8* getelementptr inbounds ([14 x i8], [14 x i8]* @0, i32 0, i32 0))
-  ret void
-}
-
-declare i32 @puts(i8*)
+	.section	".note.GNU-stack","",@progbits
